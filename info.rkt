f4dc669582eae2fe0fc81a03a3f8d407b4ecd09c
Nadeem Abdul Hamid <nadeem@acm.org>
2021-06-30 11:36:21 -0400

    update info

--- info.rkt
+++ info.rkt
@@ -1,10 +1,15 @@
 #lang info
 
 (define collection "piecrust")
-(define pkg-desc "Automated CRUD API generation")
 
-(define deps '("base"))
+(define pkg-desc "An automated (RESTful) CRUD API generator for use with the Racket web server.")
+
+(define deps '("base"
+               "db"
+               "json"))
+
 (define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
+
 (define scribblings '(("scribblings/piecrust.scrbl" ())))
 
 (define version "0.1")
