8e63aa969ff29519e4813caa7aed4ee726f12006
Nadeem Abdul Hamid <nadeem@acm.org>
2021-07-01 13:07:30 -0400

    fix info.rkt deps

--- info.rkt
+++ info.rkt
@@ -5,7 +5,7 @@
 (define pkg-desc "An automated (RESTful) CRUD API generator for use with the Racket web server.")
 
 (define deps '("base"
-               "db-lib"))
+               "db"))
 
 (define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
 
